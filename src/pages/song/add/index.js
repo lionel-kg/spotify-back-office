e3bd353701f3de110ca0063444b00fca3c1438b0
lionel-kg <lionel77350@gmail.com>
2023-11-30 13:36:08 +0100

    add audio

--- src/pages/song/add/index.js
+++ src/pages/song/add/index.js
@@ -1,43 +1,162 @@
-import React, { useState, useEffect, useRef } from 'react';
-import fakeData from '../../fakeData';
-import Input from '../../../components/Input'
+import React, { useEffect, useState } from "react";
+import Input from "../../../components/Input";
 import PageTitle from "../../../components/PageTitle";
 import Button from "../../../components/Button";
 import styles from "./index.module.scss";
+import axios from "axios";
 
 const Index = () => {
-    const [songForm, setSongForm] = useState({})
+  const [songForm, setSongForm] = useState({ title: "", audioFile: null });
+  const [data, setData] = useState({});
+  const [artist, setArtist] = useState({});
+  const [loadingData, setLoadingData] = useState(true);
+  const [loadingArtist, setLoadingArtist] = useState(true);
+  const [loadingAlbum, setLoadingAlbum] = useState(true);
+  const [loadingAudio, setLoadingAudio] = useState(true);
+  const [album, setAlbum] = useState({});
+  const [audio, setAudio] = useState({});
 
-    const handleInput = (e) => {
-        console.log(e.target.value);
-        setSongForm({ ...songForm, [e.target.name]: e.target.value });
+  const handleInput = (e) => {
+    setSongForm({ ...songForm, [e.target.name]: e.target.value });
+  };
+
+  const handleFileChange = (e) => {
+    setSongForm({ ...songForm, audioFile: e.target.files[0] });
+  };
+
+  const submitSong = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("file", songForm.audioFile);
+      const response = await axios.post(
+        "http://localhost:4001/audio/upload",
+        formData
+      );
+      setData(response.data);
+      setLoadingData(false);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error uploading audio file:", error);
     }
-  
-  
-    const submitSong = (e) => {
-        e.preventDefault();
+  };
 
-        console.log(songForm );
+  useEffect(() => {
+    const fetchData = async () => {
+      if (loadingData !== true && Object.keys(data).length > 0) {
+        const metaData = data.metaData;
+        const bodyArtist = {
+          name: metaData.artist,
+        };
+        try {
+          const newArtist = await axios.post(
+            "http://localhost:4001/artist",
+            bodyArtist
+          );
+          console.log(newArtist.data);
+          setArtist(newArtist.data);
+          setLoadingArtist(false);
+        } catch (error) {
+          console.error("Error creating artist:", error);
+        }
+      }
     };
-  
-    return (
-      <div>
-        <PageTitle title="Ajouter une musique" />
-        <form className={styles.form} onSubmit={(e) => submitSong(e)}>
-            <div className={styles.form_container}>
-                <div className={styles.form_fields}>
-                    <Input required={true} label="Title" type="text" name="name" value={songForm.name || ""} onChange={(e) => {handleInput(e);}} /> 
-                    <Input required={true} label="Album" type="text" name="album" value={songForm.album || ""} onChange={(e) => {handleInput(e);}} /> 
-                    <Input required={true} label="Artiste" type="text" name="artiste" value={songForm.artiste || ""} onChange={(e) => {handleInput(e);}} /> 
-                </div>
-                <div className={styles.form_image}>
-                    <Input required={true} label="Image" type="file" name="image" value={songForm.image || ""} onChange={(e) => {handleInput(e);}} />
-                </div>
-            </div>
-            <Button title="Ajouter une musique" type="submit" />
-        </form>
-      </div>
-    );
-}
- 
+    fetchData();
+  }, [data, loadingData]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(loadingArtist, artist, Object.keys(artist).length > 0);
+
+      if (loadingArtist !== true && Object.keys(artist).length > 0) {
+        const metaData = data.metaData;
+        console.log(metaData);
+        const bodyAlbum = {
+          title: metaData.album,
+          artistId: artist.id,
+        };
+
+        try {
+          const newAlbum = await axios.post(
+            "http://localhost:4001/album",
+            bodyAlbum
+          );
+          console.log(newAlbum.data);
+          setAlbum(newAlbum.data);
+          setLoadingAlbum(false);
+        } catch (error) {
+          console.error("Error creating album:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [artist, loadingArtist]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(
+        loadingAlbum,
+        loadingArtist,
+        album,
+        artist,
+        Object.keys(album).length > 0 && Object.keys(artist).length > 0
+      );
+
+      if (
+        loadingAlbum !== true &&
+        loadingArtist !== true &&
+        Object.keys(album).length > 0 &&
+        Object.keys(artist).length > 0
+      ) {
+        const metaData = data.metaData;
+        const audio = data.audio;
+        console.log(audio);
+        console.log(metaData);
+        const bodyAudio = {
+          title: metaData.title,
+          artistId: artist.id,
+          albumId: album.id,
+          url: audio.url,
+        };
+        console.log(bodyAudio);
+        try {
+          const newAudio = await axios.post(
+            "http://localhost:4001/audio",
+            bodyAudio
+          );
+          console.log(newAudio.data);
+          setAudio(newAudio.data);
+          setLoadingAudio(false);
+        } catch (error) {
+          console.error("Error creating audio:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [album, loadingAlbum, loadingArtist]);
+
+  return (
+    <div>
+      <PageTitle title="Ajouter une musique" />
+      <form className={styles.form} onSubmit={submitSong}>
+        <div className={styles.form_container}>
+          <div className={styles.form_fields}>
+            <Input
+              required={true}
+              label="Audio File"
+              type="file"
+              name="audioFile"
+              onChange={handleFileChange}
+            />
+          </div>
+        </div>
+        <Button title="Ajouter une musique" type="submit" />
+      </form>
+    </div>
+  );
+};
+
 export default Index;
