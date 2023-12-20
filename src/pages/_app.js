1a3f1f454762d00f0a67be6b289f183eeeaff2b8
lionel-kg <lionel77350@gmail.com>
2023-12-21 00:39:23 +0100

    fix path

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import styles from '../styles/styles.scss';
+import styles from '@/styles/styles.scss';
 import {useRouter} from 'next/router';
-import Navbar from '../components/Navbar';
+import Navbar from '@/components/Navbar';
 import localFont from 'next/font/local';
 
 const myFont = localFont({src: '../../public/fonts/GothamMedium.ttf'});
