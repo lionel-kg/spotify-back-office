f6b2249a5d6c07d80f26c251818feec5e315d373
lionel-kg <lionel77350@gmail.com>
2023-12-21 00:53:22 +0100

    change name pm2

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'next-app',
+      name: 'back-office-spotify',
       script: './node_modules/next/dist/bin/next',
       args: 'start',
       env: {
