c9ba5ab3b8bf0354d1231000baac22bf7e52cbb7
lionel-kg <lionel77350@gmail.com>
2023-12-21 10:57:42 +0100

    env variable

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'i.pinimg.com'],
   },
+  env: {
+    API_BASE_URL: 'http://localhost:4001',
+  },
 };
 
 module.exports = nextConfig;
